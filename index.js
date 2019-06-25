ccd9a5256d0bb9bb6c4901814f678681c43095aa
raybam <bamideleadedamola@gmail.com>
2019-06-25 03:16:15 +0100

    run the code

---
+++ index.js
@@ -0,0 +1,34 @@
+window.addEventListener ('load', () => {
+	const sounds=document.querySelectorAll(".sounds");
+	const pads=document.querySelectorAll(".pads div");
+	const visual=document.querySelector('.visual');
+	const colors=[
+       "#60d394",
+       "#d36036",
+       "#c060d3",
+       "#d3d1b0",
+       "#6860d3",
+       "#60b2d3"
+    ]
+    
+
+    pads.forEach ((pad,index) => {
+    	pad.addEventListener('click', function () {
+        sounds[index].currentTime=0;
+    	sounds[index].play ();
+    	
+
+    	createBubbles(index);
+    });
+});
+
+const createBubbles= (index) => {
+	const bubble=document.createElement("div");
+	visual.appendChld (bubble);
+	bubble.style.backgroundColor = colors[index];
+	bubble.style.animation = "jump 1s ease";
+	bubble.addEventListener('animationend', function () {
+		visual.removeChild(this);
+	});
+ };
+});
